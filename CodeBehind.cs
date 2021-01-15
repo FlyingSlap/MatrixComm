2cbf5599813b058553aacd5a46a162dcee25030e
Lubos Spacek <lu.spacek@gmail.com>
2021-01-15 21:01:55 +0100

    Initial commit

--- CodeBehind.cs
+++ CodeBehind.cs
@@ -1,4 +1,7 @@
-﻿using ABB.Robotics.Math;
+﻿using System.Net;
+using System.Net.Sockets;
+
+using ABB.Robotics.Math;
 using ABB.Robotics.RobotStudio;
 using ABB.Robotics.RobotStudio.Stations;
 using System;
@@ -20,6 +23,44 @@ namespace MatrixComm
     /// </remarks>
     public class CodeBehind : SmartComponentCodeBehind
     {
+        #region Properties
+
+        private IPEndPoint endPoint { get; set; }
+
+        private Socket commSocket { get; set; }
+
+        const int msgSize = 64;
+        private static byte[] msgBuffer = new byte[msgSize];
+
+        private SmartComponent _component { get; set; }
+
+        #endregion
+
+        #region Initialization
+
+        public CodeBehind()
+        {
+            // code behind initialization
+        }
+
+        private void InitializeSocket(String IP, int port)
+        {
+            endPoint = new IPEndPoint(address: IPAddress.Parse(IP), port: port);
+
+            commSocket = new Socket(endPoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            commSocket.Connect(endPoint);
+
+        }
+
+        private void CloseSocket()
+        {
+            //commSocket.Shutdown(SocketShutdown.Both);
+            commSocket.Close();
+        }
+
+        #endregion
+
+
         /// <summary>
         /// Called when the value of a dynamic property value has changed.
         /// </summary>
@@ -30,6 +71,16 @@ namespace MatrixComm
         {
         }
 
+        public override void OnLoad(SmartComponent component)
+        {
+            Logger.AddMessage(new LogMessage($"{component.Name} component loaded! Simulation must be running in order for it to work!"));
+        }
+
+        public override void OnSimulationStop(SmartComponent component)
+        {
+            component.IOSignals["Activate"].Value = false;
+        }
+
         /// <summary>
         /// Called when the value of an I/O signal value has changed.
         /// </summary>
@@ -37,6 +88,28 @@ namespace MatrixComm
         /// <param name="changedSignal"> Changed signal. </param>
         public override void OnIOSignalValueChanged(SmartComponent component, IOSignal changedSignal)
         {
+            if (changedSignal.Name.Equals("Activate"))
+            {
+                if (changedSignal.Value.Equals(1))
+                {
+                    try
+                    {
+                        InitializeSocket((String)component.Properties["IP"].Value, (int)component.Properties["port"].Value);
+                    }
+                    catch (SocketException)
+                    {
+                        component.IOSignals["Activate"].Value = false;
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }    
+                }
+                else
+                {
+                    CloseSocket();
+                }
+            }
         }
 
         /// <summary>
@@ -51,6 +124,29 @@ namespace MatrixComm
         /// </remarks>
         public override void OnSimulationStep(SmartComponent component, double simulationTime, double previousTime)
         {
+            // if activated
+            if (component.IOSignals["Activate"].Value.Equals(1))
+            {
+                // if there is data to be read
+                if (commSocket.Available > 0)
+                {
+                    commSocket.Receive(msgBuffer, msgSize, SocketFlags.None);
+
+                    var matrix = "";
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            matrix += String.Format("{0:0.0000}",BitConverter.ToSingle(msgBuffer, 4*i + j)).PadRight(20);
+                        }
+                        matrix += "\r\n";
+
+                    }
+                    component.Properties["Matrix"].Value = matrix;
+
+                    commSocket.Send(msgBuffer);
+                }
+            }
         }
     }
 }
